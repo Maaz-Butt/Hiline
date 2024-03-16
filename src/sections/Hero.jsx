35c8be185ed969234ae08e4a760232eda3a72d91
Maaz-Butt <maazb7461@gmail.com>
2024-03-16 13:18:15 +0500

    update card hover

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
     return (
-        <div className='h-[100vh] mx-auto max-w-8xl px-2 sm:px-6 lg:px-8 text-center flex justify-center items-center flex-col'>
+        <div className='my-3 mx-auto max-w-8xl px-2 sm:px-6 lg:px-8 text-center flex justify-center items-center flex-col'>
             <h1 className='text-6xl font-bold pt-10 pl-10' ><span className='text-orange-600' >Renovation</span> Services</h1>
             <p className='pt-5 lg:px-48 md:px-16 tracking-wide'>Welcome to <span className='text-orange-600'> Hiline </span>, your trusted interior design and architecture company in Lahore! We specialize in providing top-notch renovation services for a
                 wide range of spaces, including homes, commercial plazas, offices, and various other commercial and residential properties. Our team of skilled
